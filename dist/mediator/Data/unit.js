69020fccf6aa0d15a68714cf5da1f0db585be073
Omri Hadur <omri.hadur@gmail.com>
2023-03-07 23:22:53 +0200

    Delete dist directory

--- dist/mediator/Data/unit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Unit {
-}
-exports.default = Unit;
-Unit.Instance = new Unit();
--- dist/messeges/testMessege.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class TestRequest {
-    constructor(name, number) {
-        this.name = name;
-        this.number = number;
-    }
-}
-exports.default = TestRequest;
